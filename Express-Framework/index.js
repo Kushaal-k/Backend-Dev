e344d88811ba811e51bb51dc460fd7bd2b6bea63
Kushaal Kankane <kushaalkankane12345@gmail.com>
2026-02-06 01:23:39 +0530

    [Kushaal] Add put and delete methods

--- Express-Framework/index.js
+++ Express-Framework/index.js
@@ -6,18 +6,29 @@ app.use(express.json())
 const PORT = 8000;
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the home page.")
+    res.status(200).send("Welcome to the home page.")
 })
 
 app.get("/students", (req, res) => {
     const branch = req.query.branch;
 
-    if(!branch){
-        res.json(students);
-        return;
-    }
+    fs.readFile("students.json", "utf-8", (err, data) => {
+        if(err) {
+            return res.status(500).send("Error while accessing database!")
+        }
+        const students = JSON.parse(data || [])
 
-    res.json(students.filter(s => s.branch === branch))
+        if(students.length === 0) {
+            return res.status(404).send("Students not found");
+        }
+
+        if(!branch){
+            return res.status(200).json(students);
+        }
+
+        return res.status(200).json(students.filter(s => s.branch === branch))
+    })
+    
 })
 
 app.get("/students/:id", (req, res) => {
@@ -36,7 +47,7 @@ app.get("/students/:id", (req, res) => {
             return res.status(404).send("Student not found");
         }
 
-        return res.status(202).json(student)
+        return res.status(200).json(student)
     })
 })
 
@@ -51,10 +62,10 @@ app.post("/students/register", (req, res) => {
             return res.status(500).send("Error while accessing database!")
         }
 
-        
+        const students = JSON.parse(data);
 
         students.push({
-            id: crypto.randomUUID(),
+            id: students.length > 0 ? students[students.length - 1].id + 1 : 1,
             name,
             branch
         })
@@ -66,6 +77,63 @@ app.post("/students/register", (req, res) => {
         })
 
         return res.status(200).json(students)
+    })
+})
+
+app.delete("/students/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    fs.readFile("students.json", "utf-8", (err, data) => {
+        if(err){
+            return res.status(500).send("Error while accessing database!")
+        }
+
+        const students = JSON.parse(data);
+
+        const student = students.find(s => s.id === id);
+
+        if(!student) {
+            return res.status(404).send("Student not found");
+        }
+
+        const updatedStudents = students.filter((s) => s.id !== id)
+
+        fs.writeFile("students.json", JSON.stringify(updatedStudents, null, 2), (err) => {
+            if(err){
+                return res.status(500).send("Error while saving data")
+            }
+
+            return res.status(200).json(updatedStudents);
+        })
+    })
+})
+
+app.patch("/students/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    // const {updatedName} = req.body;
+
+    fs.readFile("students.json", "utf-8", (err, data) => {
+        if(err){
+            return res.status(500).send("Error while accessing database!")
+        }
+
+        const students = JSON.parse(data);
+
+        const student = students.find(s => s.id === id);
+
+        if(!student) {
+            return res.status(404).send("Student not found");
+        }
+
+        const updatedStudents = students.map((s) => s.id === id ? {...s, ...req.body, id: s.id} : s);
+
+        fs.writeFile("students.json", JSON.stringify(updatedStudents, null, 2), (err) => {
+            if(err){
+                return res.status(500).send("Error while saving data")
+            }
+
+            return res.status(200).json(updatedStudents);
+        })
     })
 })
 
